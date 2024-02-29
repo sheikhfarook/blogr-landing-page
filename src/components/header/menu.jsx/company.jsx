d67db7be5ef6bfbae80ec5a9bc67cbb05fa69776
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-29 10:44:28 +0530

    update the comapny.jsx file

--- src/components/header/menu.jsx/company.jsx
+++ src/components/header/menu.jsx/company.jsx
@@ -1,5 +1,46 @@
+import arrow from "../../../assets/icon-arrow-light.svg";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
 const Company = () => {
-  return <div>company</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <Menu>
+            <MenuButton _hover={{ textDecoration: "underline" }}>
+              <div className="flex place-items-end  gap-2">
+                <div>company</div>
+                <div>
+                  <img className="mb-0.5" src={arrow} alt="" />
+                </div>
+              </div>
+            </MenuButton>
+            <MenuList
+              color="black"
+              fontWeight="400"
+              py="10"
+              px="5"
+              minWidth="13rem"
+              mt="2rem"
+              ml="-1.5rem">
+              <MenuItem className="hover:font-[700]">About </MenuItem>
+              <MenuItem className="hover:font-[700]">Team </MenuItem>
+              <MenuItem className="hover:font-[700]">Blog </MenuItem>
+              <MenuItem className="hover:font-[700]">Careers</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Company;
