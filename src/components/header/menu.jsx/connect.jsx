9db56d9c45e114e017ab9e501c172d1f65a5aebe
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-29 10:44:44 +0530

    update the connect.jsx file

--- src/components/header/menu.jsx/connect.jsx
+++ src/components/header/menu.jsx/connect.jsx
@@ -1,5 +1,47 @@
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
+
+import arrow from "../../../assets/icon-arrow-light.svg";
+
 const Connect = () => {
-  return <div>connect</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <Menu>
+            <MenuButton _hover={{ textDecoration: "underline" }}>
+              <div className="flex place-items-end   gap-2  ">
+                <div>connect</div>
+                <div>
+                  <img className="mb-0.5" src={arrow} alt="" />
+                </div>
+              </div>
+            </MenuButton>
+            <MenuList
+              color="black"
+              fontWeight="400"
+              py="10"
+              px="5"
+              minWidth="13rem"
+              mt="2rem"
+              ml="-1.5rem">
+              <MenuItem className="hover:font-[700]"> Contact</MenuItem>
+              <MenuItem className="hover:font-[700]">Newsletter</MenuItem>
+              <MenuItem className="hover:font-[700]"> Linkedin</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Connect;
