f77cf867cb3f9fd7e9d2c45f4db10038e7f510c9
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-29 15:12:22 +0530

    import futureDesigned file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import HeaderTopBar from "./components/header/headertopbar";
+import FutureDesigned from "./components/main/futureDesigned";
 
 function App() {
   return (
     <div>
       <HeaderTopBar />
+      <FutureDesigned />
     </div>
   );
 }
