3e55bddecfd4e43849aaf6e797f3eda4fbe01ac3
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-29 12:27:44 +0530

    update the code

--- src/components/header/startfree.jsx
+++ src/components/header/startfree.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-
 const StartFree = () => {
-  return <div></div>;
+  return (
+    <div className="p-10">
+      <div>
+        <div className="w-[920px] h-[81.02px] text-white text-[64px] font-[600] text-center tracking-[-1.92px]">
+          A modern publishing platform
+        </div>
+        <div>Grow your audience and build your online brand</div>
+      </div>
+      {/* <div className="flex">
+        <div
+          className="w-[137px] h-[48px] rounded-[28px] shadow-md flex items-center 
+                            justify-center cursor-pointer bg-[#fff]
+             mx-auto text-[#ff505c] hover:text-[#fff]  hover:bg-[#ff7b86] ">
+          Sign Up
+        </div>
+        <div
+          className="w-[137px] h-[48px] rounded-[28px] shadow-md flex items-center 
+                            justify-center cursor-pointer 
+             mx-auto text-[#fff] hover:text-[#fff]  hover:bg-[#FFF] ">
+          Sign Up
+        </div>
+      </div> */}
+    </div>
+  );
 };
 
 export default StartFree;
