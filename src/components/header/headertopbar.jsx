12921e0bdeea639b2188b5cdae469a0a766ed9cf
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-29 14:37:30 +0530

    complete the header part

--- src/components/header/headertopbar.jsx
+++ src/components/header/headertopbar.jsx
@@ -7,19 +7,21 @@ import StartFree from "./startfree";
 const HeaderTopBar = () => {
   return (
     // main bg color
-    <div className="bg-gradient-to-r from-[#ff8f71] to-[#ff3e55]">
+    <div className="bg-gradient-to-r from-[#ff8f71] to-[#ff3e55] hover:rounded-bl-[6rem] max-sm:rounded-bl-[6rem]   ">
       {/* bg img css name */}
       <div className="blog ">
-        <div className="flex items-center justify-between p-10 text-white text-[16px] font-ubuntu font-[700]">
+        <div className="flex items-center justify-between p-10 xl:px-36 text-white text-[16px] font-ubuntu font-[700]">
           {/* logo */}
-          <div
-            className="flex gap-[5rem] items-center   
-         ">
+          <div className="flex gap-[5rem] items-center">
             <div>
-              <img className="" src={blog} alt="" />
+              <img
+                className="max-sm:w-[81.66px] max-sm:h-[32px]"
+                src={blog}
+                alt=""
+              />
             </div>
             {/* menu bar */}
-            <div className="flex gap-10 ">
+            <div className="flex gap-10  max-sm:hidden">
               <div>
                 <Product />
               </div>
@@ -32,12 +34,12 @@ const HeaderTopBar = () => {
               {/* right side  */}
             </div>
           </div>
-          <div className="flex gap-10 items-center">
+          <div className="flex gap-6 items-center cursor-pointer max-sm:hidden">
             <div> Login </div>
             <div
               className="w-[137px] h-[48px] rounded-[28px] shadow-md flex items-center 
-                            justify-center cursor-pointer bg-[#fff]
-             mx-auto text-[#ff505c] hover:text-[#fff]  hover:bg-[#ff7b86] ">
+              justify-center  bg-[#fff] mx-auto text-[#ff505c] hover:text-[#fff] 
+               hover:bg-[#ff7b86] ">
               Sign Up
             </div>
           </div>
--- src/components/header/startfree.jsx
+++ src/components/header/startfree.jsx
@@ -1,11 +1,39 @@
 const StartFree = () => {
   return (
-    <div className="p-10">
-      <div>
-        <div className="w-[920px] h-[81.02px] text-white text-[64px] font-[600] text-center tracking-[-1.92px]">
+    <div className="m-auto max-sm:my-12 md:my-20 ">
+      <div className="text-center space-y-5 font-overpass text-white max-sm:space-y-8">
+        <div
+          className="font-[600] m-auto tracking-[-1.92px] 
+          max-sm:text-[36px] max-sm:w-[320px] max-sm:h-[91.15px] max-sm:tracking-[-1.08px] 
+          md:text-[40px] md:w-[500px] md:h-[45px] 
+          lg:h-[81.02px] lg:w-[920px] lg:text-[64px] ">
           A modern publishing platform
         </div>
-        <div>Grow your audience and build your online brand</div>
+        <div
+          className=" font-[300]  m-auto
+        max-sm:text-[18px] max-sm:w-[327px]
+          md:text-[14px] md:w-[350px]
+          lg:text-[20px] lg:w-[423px] lg:h-[26px] ">
+          Grow your audience and build your online brand
+        </div>
+      </div>
+      {/* buttons */}
+
+      <div
+        className="flex gap-6 text-[16px] font-[700] m-auto justify-center font-ubuntu
+      max-sm:mt-10  md:mt-16 lg:mt-20 
+       ">
+        <div
+          className="w-[137px] h-[48px] rounded-[28px] flex justify-center items-center bg-white
+          cursor-pointer text-[#ff505c] hover:text-white hover:bg-[#ff7b86]
+          ">
+          Start for Free
+        </div>
+        <div
+          className="w-[137px] h-[48px] rounded-[28px] flex justify-center items-center border-[1px] 
+        cursor-pointer border-white text-white hover:text-[#ff7b86] hover:bg-[#FFF] ">
+          Learn More
+        </div>
       </div>
       {/* <div className="flex">
         <div
