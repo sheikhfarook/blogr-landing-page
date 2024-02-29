e8d6e8ffe2c1aac9da8e9d1d467d1f4627ce563d
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-29 10:45:02 +0530

    update product.jsx file

--- src/components/header/menu.jsx/product.jsx
+++ src/components/header/menu.jsx/product.jsx
@@ -1,9 +1,46 @@
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
+
+import arrow from "../../../assets/icon-arrow-light.svg";
+
 const Product = () => {
   return (
     <div>
       <div>
-        <div>product</div>
-        <div></div>
+        <div>
+          <Menu>
+            <MenuButton _hover={{ textDecoration: "underline" }}>
+              <div className="flex place-items-end  gap-2">
+                <div>Product</div>
+                <div>
+                  <img className="mb-0.5" src={arrow} alt="" />
+                </div>
+              </div>
+            </MenuButton>
+            <MenuList
+              color="black"
+              fontWeight="400"
+              py="10"
+              px="5"
+              minWidth="13rem"
+              mt="2rem"
+              ml="-1.5rem">
+              <MenuItem className="hover:font-[700]"> Overview </MenuItem>
+              <MenuItem className="hover:font-[700]">Pricing</MenuItem>
+              <MenuItem className="hover:font-[700]"> Marketplace</MenuItem>
+              <MenuItem className="hover:font-[700]">Features</MenuItem>
+              <MenuItem className="hover:font-[700]">Integrations</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </div>
     </div>
   );
