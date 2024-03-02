be1c3d136a3fc6e4a67b1110ff50a9145d6464e6
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-03-02 18:01:32 +0530

    create the contact data

---
+++ src/components/footer/contactData.jsx
@@ -0,0 +1,17 @@
+export const BlogrData = [
+  {
+    id: 1,
+    header: "Product",
+    options: ["Overview", "Pricing", "Marketplace", "Features", "Integrations"],
+  },
+  {
+    id: 2,
+    header: "Company",
+    options: ["About", "Team", "Blog", "Careers"],
+  },
+  {
+    id: 3,
+    header: "Connect",
+    options: ["Contact", "Newsletter", "LinkedIn"],
+  },
+];
