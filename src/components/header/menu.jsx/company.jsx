befdc9e62e3b228089d66d46aeddf204d0d5c5b3
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-03-03 16:33:15 +0530

    complete the company jsx file

--- src/components/header/menu.jsx/company.jsx
+++ src/components/header/menu.jsx/company.jsx
@@ -1,25 +1,45 @@
 import arrow from "../../../assets/icon-arrow-light.svg";
+// import arrows from "../../../assets/icon-arrow-dark.svg";
 import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
 } from "@chakra-ui/react";
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 const Company = () => {
   return (
     <div>
       <div>
-        <div>
+        <div className="md:hidden">
+          <Accordion allowMultiple>
+            <AccordionItem>
+              <h2>
+                <AccordionButton color="#ff8f71">
+                  <Box color="black">
+                    <div className="">company</div>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel opacity="0.5">
+                <div>About</div>
+                <div>Team</div>
+                <div>Blog</div>
+                <div>Careers</div>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <div className="max-sm:hidden">
           <Menu>
             <MenuButton _hover={{ textDecoration: "underline" }}>
               <div className="flex place-items-end  gap-2">
                 <div>company</div>
                 <div>
-                  <img className="mb-0.5" src={arrow} alt="" />
+                  <img className="mb-0.5 max-sm:hidden" src={arrow} alt="" />
                 </div>
               </div>
             </MenuButton>
